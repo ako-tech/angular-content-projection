343417cb2e1015e477c656d7d1823128ff71606d
ako-tech <akotech20@gmail.com>
2022-11-09 10:00:36 +0100

    ref to use directives as projection tokens

--- src/app/expansion-panel/expansion-panel.module.ts
+++ src/app/expansion-panel/expansion-panel.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExpansionPanelComponent } from './expansion-panel.component';
 import { ExpansionPanelSummaryComponent } from './expansion-panel-summary/expansion-panel-summary.component';
+import {
+  ExpansionPanelDetailsContentDirective,
+  ExpansionPanelSummaryContentDirective,
+} from './directives';
 
 @NgModule({
-  declarations: [ExpansionPanelComponent, ExpansionPanelSummaryComponent],
+  declarations: [
+    ExpansionPanelComponent,
+    ExpansionPanelSummaryComponent,
+    ExpansionPanelSummaryContentDirective,
+    ExpansionPanelDetailsContentDirective,
+  ],
   imports: [CommonModule],
-  exports: [ExpansionPanelComponent],
+  exports: [
+    ExpansionPanelComponent,
+    ExpansionPanelSummaryContentDirective,
+    ExpansionPanelDetailsContentDirective,
+  ],
 })
 export class ExpansionPanelModule {}
