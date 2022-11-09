f089184497fdcc23abdedc52eeb0d7bb171a60ae
ako-tech <akotech20@gmail.com>
2022-11-09 09:16:35 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ako-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'content-projection';
+}
